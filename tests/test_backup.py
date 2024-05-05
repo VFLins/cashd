8df7b521a0761efa3a40700c2b61d382524d0de2
VFLins <vflisn@live.com>
2024-05-05 10:21:24 -0300

    uptade test

--- tests/test_backup.py
+++ tests/test_backup.py
@@ -1,22 +1,21 @@
-import unittest
 from configparser import ConfigParser
 from tempfile import TemporaryFile
 from cashd.backup import (
     get_db_size,
-    write_current_size,
+    write_current_size
 )
 
 
 def test_dbsize_read():
-    with TemporaryFile() as data_file, TemporaryFile() as config_file:
+    with TemporaryFile(delete=False) as data_file, TemporaryFile(delete=False) as config_file:
         data_file.write(b"testando um teste testoso...")
 
         write_current_size(
             config_file=config_file.name,
             current_size=get_db_size(file_path=data_file.name))
         
-        parser = ConfigParser()
-        parser.read(config_file.name)
-        read_dbsize = int(parser["file_sizes"]["dbsize"])
+        config = ConfigParser()
+        config.read(config_file.name)
+        read_dbsize = int(config["file_sizes"]["dbsize"])
 
         assert  read_dbsize == get_db_size(file_path=data_file.name)
