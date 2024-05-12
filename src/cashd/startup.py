c78ccfd8ddfd16981421ebccc9a5f628c327cc88
VFLins <vflisn@live.com>
2024-05-12 12:45:26 -0300

    update startup command

--- src/cashd/startup.py
+++ src/cashd/startup.py
@@ -3,6 +3,10 @@ import sys
 
 
 def window():
+    if len(sys.argv) == 1:
+        start_cashd(with_webview=True)
+        return
+
     if "window" in sys.argv:
         start_cashd(with_webview=True)
         return
