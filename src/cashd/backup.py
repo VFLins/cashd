dec193aebbcfcc0c4a7d30d17032a43207e9374a
Vitor Ferreira Lins <vflins@live.com>
2024-05-13 11:38:09 -0300

    add functionality for loading database

--- src/cashd/backup.py
+++ src/cashd/backup.py
@@ -1,4 +1,7 @@
-from os import path, makedirs, remove
+from cashd.db import DB_ENGINE
+
+import sqlite3
+from os import path, makedirs, rename
 from datetime import datetime
 import shutil
 import configparser
@@ -73,9 +76,47 @@ def copy_file(source_path, target_dir, _raise: bool = False):
         filename = f"backup_{now}.db".replace(":", "-")
         shutil.copyfile(source_path, path.join(target_dir, filename))
         logger.info(f"Copia de '{source_path}' criada em '{target_dir}'")
-
     except FileNotFoundError as xpt:
-        logger.error(f"Erro realizando copia: {xpt}.")
+        logger.error(f"Erro realizando copia: {xpt}.", exc_info=1)
+        if _raise: raise xpt
+
+
+def rename_on_db_folder(current: str, new: str, _raise: bool = False):
+    logger.debug("function call: rename_on_db_folder")
+    current, new = str(current), str(new)
+
+    db_folder = path.split(DB_FILE)[0]
+    path_to_current = path.join(db_folder, current)
+    path_to_new = path.join(db_folder, new)
+    
+    try:
+        rename(path_to_current, path_to_new)
+        logger.info(f"{path_to_current} renomeado como {path_to_new}")
+    except Exception as xpt:
+        logger.error(f"Erro renomeando {path_to_current}: {xpt}", exc_info=1)
+        if _raise: raise xpt
+
+
+def check_sqlite(file: str, _raise: bool = False):
+    """Checa se o full path `file` representa um banco de dados."""
+    logger.debug("function call: check_sqlite")
+
+    if not path.isfile(file):
+        xpt = FileExistsError(f"Arquivo {file} invalido.")
+        logger.error(str(xpt))
+        if _raise: raise xpt
+        return
+    
+    con = sqlite3.connect(file)
+    cursor = con.cursor()
+    stmt = f"PRAGMA integrity_check;"
+    try:
+        _ = cursor.execute(stmt).fetchone()
+        return True
+    except sqlite3.DatabaseError:
+        return False
+    except Exception as xpt:
+        logger.critical(f"Erro inesperado validando {file}", exc_info=1)
         if _raise: raise xpt
 
 
@@ -88,7 +129,6 @@ def read_db_size(file_path: str = DB_FILE) -> int:
     try:
         size = path.getsize(file_path)
         return size
-
     except FileNotFoundError:
         logger.error(f"Arquivo '{file_path}' não encontrado.")
         return None
@@ -101,7 +141,6 @@ def read_last_recorded_size(config_file: str = CONFIG_FILE):
 
     if "file_sizes" in config:
         return config["file_sizes"].getint("dbsize", fallback=None)
-
     return 0
 
 
@@ -178,6 +217,17 @@ def write_rm_backup_place(idx: int):
         conf.write(newconfig)
 
 
+def load(file: str, _raise: bool = False) -> None:
+    """
+    Checa se `file` se trata de um banco de dados SQLite valido, e entao vai
+    traze-lo para a pasta local do banco de dados do Cashd.
+    
+    Se um banco de dados ja estiver presente, vai renomea-lo para um nome
+    nao usado pelo Cashd nem por outros arquivos na pasta e o mantera no
+    diretorio.
+    """
+
+
 def run(force: bool = False, _raise: bool = False) -> None:
     """
     Vai fazer a copia do arquivo de banco de dados para a pasta local de backup
@@ -204,11 +254,11 @@ def run(force: bool = False, _raise: bool = False) -> None:
             try:
                 copy_file(DB_FILE, place, _raise=True)
             except Exception as xpt:
-                logger.error(f"Nao foi possivel salvar em '{place}': {xpt}")
+                logger.error(f"Nao foi possivel salvar em '{place}': {xpt}", exc_info=1)
                 if _raise: error_was_raised = True
         if error_was_raised:
             raise NotADirectoryError(
                 f"Erro em alguma etapa do backup, verifique o log: {LOG_FILE}")
     except Exception as xpt:
-        logger.error(f"Erro inesperado durante o backup: {xpt}")
+        logger.error(f"Erro inesperado durante o backup: {xpt}", exc_info=1)
     
