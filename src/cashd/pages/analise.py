008df8fabdc5d67b57e507a0fef5e0e47cdb629e
Vitor F. Lins <vflins@live.com>
2024-06-10 11:12:53 -0300

    update style statistics page

--- src/cashd/pages/analise.py
+++ src/cashd/pages/analise.py
@@ -18,22 +18,22 @@ PG_ANALISE = """
 
 
 ELEM_MAIN = """
-<|layout|columns=1fr 1fr 2fr|class_name=header_container|
+<|layout|columns=1fr 1fr 2fr|class_name=top_controls|
 
 
 <|{dropdown_tipo_val}|selector|lov={dropdown_tipo_lov}|dropdown|>
 
 <|{dropdown_periodo_val}|selector|lov={dropdown_periodo_lov}|dropdown|>
 
-<|{slider_val}|slider|lov={slider_lov}|text_anchor=none|>
+<|{slider_val}|slider|lov={slider_lov}|text_anchor=botom|>
 
 |>
-
-<br />
 
 <center>
 <|Atualizar|button|on_action={btn_gerar_main_plot}|>
 </center>
 
-<|chart|figure={main_plot}|>
+<br />
+
+<|chart|figure={main_plot}|height=360px|>
 """
