1eb33cf6533ecacedfd7d124f1b9c77a29b71ae9
Vitor F. Lins <vflins@live.com>
2024-08-08 16:33:38 -0300

    feature: escrever arquivo de preferencias com valores padrao

--- src/cashd/prefs.py
+++ src/cashd/prefs.py
@@ -22,6 +22,10 @@ for file in [CONFIG_FILE, LOG_FILE]:
 
 conf = configparser.ConfigParser()
 conf.read(CONFIG_FILE, "utf-8")
+try:
+    conf.add_section("default")
+except configparser.DuplicateSectionError:
+    pass
 
 logger = logging.getLogger("cashd.prefs")
 logger.setLevel(logging.DEBUG)
@@ -38,15 +42,18 @@ logger.addHandler(log_handler)
 def write_uf_preferido(val: str):
     """Define `val` como o valor no campo [Default] `uf_preferido`"""
     logger.debug("function call: write_uf_preferido")
-    config = configparser.ConfigParser()
-    config.read(CONFIG_FILE, "utf-8")
-            
-    config["default"] = {"uf_preferido": val}
-    with open(CONFIG_FILE, "w") as newconfig:
-        config.write(newconfig)
+
+    try:
+        conf.set("default", "uf_preferido", val)
+        with open(CONFIG_FILE, "a") as newconfig:
+            conf.write(newconfig)
+        conf.read(CONFIG_FILE, "utf-8")
+    except Exception as xpt:
+        logger.error(f"Erro inesperado: {str(xpt)}")
+        raise xpt
 
 
-def read_uf_preferido() -> str:
+def read_uf_preferido() -> str | None:
     """Retorna o valor de `uf_preferido` em 'prefs.ini' como `str`"""
     logger.debug("function call: read_uf_preferido")
     config = configparser.ConfigParser()
@@ -55,4 +62,49 @@ def read_uf_preferido() -> str:
     try:
         return config.get("default", "uf_preferido")
     except configparser.NoSectionError:
-        return ""
+        return None
+    except configparser.NoOptionError:
+        return None
+
+
+def write_limite_ultimas_transacs(val: int):
+    """Define `val` como o valor no campo [Default] `limite_ultimas_transacs`"""
+    logger.debug("function call: write_limite_ultimas_transacs")
+
+    try:
+        val = int(val)
+        conf.set("default", "limite_ultimas_transacs", str(val))
+        with open(CONFIG_FILE, "w") as newconfig:
+            conf.write(newconfig)
+        conf.read(CONFIG_FILE, "utf-8")
+    except ValueError as xpt:
+        msg = f"Esperado que `val` seja do tipo `int`"
+        logger.error(msg)
+        raise ValueError(msg)
+    except Exception as xpt:
+        msg = f"Erro inesperado: {str(xpt)}"
+        logger.error(msg)
+        raise xpt
+
+
+def read_limite_ultimas_transacs() -> int | None:
+    """Retorna o valor de `limite_ultimas_transacs` em 'prefs.ini' como `int`"""
+    logger.debug("function call: read_limite_ultimas_transacs")
+
+    try:
+        return conf.getint("default", "limite_ultimas_transacs")
+    except configparser.NoSectionError:
+        return None
+    except configparser.NoOptionError:
+        return None
+
+
+######################
+### write defaults ###
+######################
+
+if not read_uf_preferido():
+    write_uf_preferido("AC")
+
+if not read_limite_ultimas_transacs():
+    write_limite_ultimas_transacs(1000)
