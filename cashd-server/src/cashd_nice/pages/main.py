81fdc7ee7109d0a9b1a3b5ed7d6dd27752b88fbc
Vitor <vflins@live.com>
2026-05-01 14:00:16 -0300

    add: right column elements

--- cashd-server/src/cashd_nice/pages/main.py
+++ cashd-server/src/cashd_nice/pages/main.py
@@ -1,16 +1,70 @@
+from datetime import date
+
+from cashd_core.const import ESTADOS
 from cashd_nice.widgets import DefaultHeader, DetailedList
 
 
 def subpage_transac(ui):
-    ui.label("Aqui se adicionam as transações")
+    with ui.column(align_items="end"):
+        dateinput = ui.date_input("Data", value=date.today().strftime("%d/%m/%Y"))
+        dateinput.picker.props("mask='DD/MM/YYYY'")
+        dateinput.classes("w-40")
+        ui.input("Valor", placeholder="0,00").classes("w-40")
+        ui.button("Inserir")
 
 
 def subpage_history(ui):
-    ui.label("Aqui se vê o histórico de transações")
+    ui.add_head_html(
+        """
+        <script>
+        function langAgnosticPageIndicator(firstRowIndex, endRowIndex, rowsNumber) {
+            return firstRowIndex + '-' + endRowIndex + ' [' + rowsNumber + ']';
+        }
+        </script>
+        """
+    )
+    with ui.column(align_items="end"):
+        ui.button("Imprimir", icon="print")
+        table = ui.table(
+            row_key="id",
+            columns=[
+                {"name": "data", "label": "Data", "field": "data"},
+                {"name": "valor", "label": "Valor (R$)", "field": "valor"},
+                {"name": "action", "label": ""},
+            ],
+            rows=[
+                {"id": 1, "data": "12/10/2024", "valor": "223,30"},
+                {"id": 2, "data": "11/01/2025", "valor": "35,50"},
+                {"id": 3, "data": "22/03/2024", "valor": "-200,00"},
+            ],
+            pagination=5,
+        ).props(
+            "rows-per-page-label='Linhas por página:' "
+            ":pagination-label='langAgnosticPageIndicator'"
+        )
+        with table.add_slot("body-cell-action"):
+            with table.cell("action"):
+                del_button = ui.button(icon="delete").props("flat size=0.7em")
+                del_button.on(
+                    "click",
+                    js_handler="() => emit(props.row.id)",
+                    handler=lambda e: ui.notify(f"Excluindo transação id={e.args}")
+                )
 
 
 def subpage_info(ui):
-    ui.label("Aqui se consulta dados do cadastro do cliente")
+    with ui.grid(columns=2):
+        ui.input("Nome*").classes("w-40")
+        ui.input("Sobrenome*").classes("w-40")
+        ui.input("Apelido").classes("w-40")
+        ui.input("Telefone").classes("w-40")
+        ui.input("Endereço").classes("w-40")
+        ui.input("Bairro").classes("w-40")
+        ui.input("Cidade").classes("w-40")
+        ui.select(ESTADOS, value=ESTADOS[0], label="Estado").classes("w-40")
+    with ui.row():
+        ui.button(icon="refresh").disable()
+        ui.button(icon="check").disable()
 
 
 def page(ui):
@@ -44,7 +98,7 @@ def page(ui):
                 with ui.row().classes("gap-0"):
                     ui.button("anterior").classes("text-xs").props("flat")
                     ui.button("próximo").classes("text-xs").props("flat")
-        with ui.column().classes("overflow-hidden"):
+        with ui.column().classes("overflow-hidden w-full items-center"):
             with ui.tabs().classes("w-full").props("no-caps") as tabs:
                 transac = ui.tab("Transação")
                 history = ui.tab("Histórico")
