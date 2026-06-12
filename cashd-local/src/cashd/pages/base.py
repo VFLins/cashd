2185ba1a8661008e6918fb640c9339cbb70b6721
Vitor F. Lins <vflins@live.com>
2026-06-12 11:41:22 -0300

    add: assinchornously rearrange and ensure page size

--- cashd-local/src/cashd/pages/base.py
+++ cashd-local/src/cashd/pages/base.py
@@ -22,8 +22,8 @@ class BaseSection:
         while True:
             try:
                 await sleep(1 / 30)
-                self.ensure_window_size()
-                self.rearrange_widgets()
+                await self.ensure_window_size()
+                await self.rearrange_widgets()
             except CancelledError:
                 break
                 raise
@@ -38,13 +38,13 @@ class BaseSection:
         except AttributeError:
             return const.MAIN_WINDOW_SIZE
 
-    def rearrange_widgets(self):
+    async def rearrange_widgets(self):
         """Rearranges this section's widgets, should be used to turn this section
         responsive to the window size.
         """
         return
 
-    def ensure_window_size(self):
+    async def ensure_window_size(self):
         """Restores window size to a value above the minimum values defined at
         the application's main window's `min_height` and `min_width`.
         """
